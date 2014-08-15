8a8100a270a69a6293254b82102dc46e0ef8ccce
Nitin Labhishetty <nitinlabhishetty@gmail.com>
2014-08-16 01:52:52 +0530

    Added keystroke file
    First addition of keystroke.js file. Currently containing very crude
    outline.

---
+++ keystroke.js
@@ -0,0 +1,61 @@
+//Constructor defining KeystrokeData object structure
+function KeystrokeData () {
+    this.text = ''; //the typed text
+    //arrays holding timestamp
+    this.monographTime = []; 
+	this.digraphUUTime = [];
+	this.digraphUDTime = [];
+	this.digraphDUTime = [];
+	this.digraphDDTime = [];
+	this.trigraphUUTime = [];
+	this.trigraphUDTime = [];
+	this.trigraphDUTime = [];
+	this.trigraphDDTime = [];
+	//arrays holding list of mono,di and trigraphs
+    this.monograph = [];
+	this.digraphUU = [];
+	this.digraphUD = [];
+	this.digraphDU = [];
+	this.digraphDD = [];
+	this.trigraphUU = [];
+	this.trigraphUD = [];
+	this.trigraphDU = [];
+	this.trigraphDD = [];
+}
+
+//member functions of KeystrokeData
+KeystrokeData.prototype = {
+    constructor: KeystrokeData,
+    //input functions
+    listen:function(textFieldID){
+    	//add keyup and keydown listeners here
+    	$('#'+textFieldID).keyup(function(event){
+    		this.monograph.push(event.which);
+    		this.monographTime.push(event.timeStamp);
+    	})
+    	.keydown(function(event){
+    		this.monographTime.push(event.timeStamp);
+    	});
+    },
+    trainModelFromField:function(elementID,algorithm){},
+    trainModelFromFile:function(datasetFilepath){},
+    loadModelFromFile:function(filepath){},
+    //output functions
+    saveModelToFile:function(){},
+    getDataToCSV:function(){},
+    getJSON:function(){},
+    printStats:function(){},
+    //utility functions
+    testModel:function(){},
+    setSecurityToField:function(elementID){},
+    //algorithmic functions
+    arrayDisorder:function(){},
+    neuralNetwork:function(){}
+}
+
+//sample usage
+var myKeystrokeData = new KeystrokeData();
+myKeystrokeData.trainModelFromField('textFieldID','arrayDisorder');
+myKeystrokeData.testModel();
+console.log(myKeystrokeData.text);
+myKeystrokeData.printStats();
